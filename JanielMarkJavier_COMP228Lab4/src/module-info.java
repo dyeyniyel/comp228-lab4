f9af96e34a3abbb514c62bda337387d90c1e34d3
dyeyniyel <158533198+dyeyniyel@users.noreply.github.com>
2024-03-20 14:36:56 -0400

    Add files via upload

---
+++ JanielMarkJavier_COMP228Lab4/src/module-info.java
@@ -0,0 +1,7 @@
+module JanielMarkJavier_COMP228Lab4 {
+	requires javafx.controls;
+	requires javafx.graphics;
+	
+//	opens application to javafx.graphics, javafx.fxml;
+	exports exercise1 to javafx.graphics;
+}
